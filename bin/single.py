6d61077e922f48d2e6ea20bc71b40b6d4dd727be
Vadim Graboys <dimva13@gmail.com>
2011-09-20 21:45:51 -0400

    add simple commandline trader for single orders

---
+++ bin/single.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# adds a single order
+
+import args
+import cmd
+
+bitfloor = args.get_rapi()
+
+def trade(side, arg):
+    try:
+        size, price = arg.split()
+        print bitfloor.order_new(side=side, size=size, price=price)
+    except:
+        print "Invalid arg {1}, expected size price".format(side, arg)
+
+class Shell(cmd.Cmd):
+    prompt = '(buy|sell size price) '
+
+    def do_sell(self, arg):
+        trade(1, arg)
+
+    def do_buy(self, arg):
+        trade(0, arg)
+
+Shell().cmdloop()
